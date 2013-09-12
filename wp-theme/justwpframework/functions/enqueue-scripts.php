78fdf47b778625a6ce606c97a5a8b4f0cd086877
kharissulistiyo <kharisblank@gmail.com>
2013-09-13 03:34:20 +0700

    WP theme files
    Functions to eunqueue script files

---
+++ wp-theme/justwpframework/functions/enqueue-scripts.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+
+/*
+ * Function to register front-end scripts
+ * ====================================== */
+
+if ( ! function_exists( 'justwpframework_enqueue_script' ) ) :
+
+
+function justwpframework_enqueue_script(){
+
+	/* Register Scripts */
+	wp_register_script('modernizr', get_template_directory_uri().'/assets/js/modernizr.min.js', null, '2.6.2', false );
+
+	
+	
+	/* Enqueue Scripts */
+	wp_enqueue_script('modernizr');	
+
+}
+
+add_action( 'wp_enqueue_scripts', 'justwpframework_enqueue_script' );
+
+endif;
