f7c397bc8ea210a17cd15a41b641f952b65b5ce9
kharissulistiyo <kharisblank@gmail.com>
2013-09-13 02:24:21 +0700

    Add headings

--- text-format.php
+++ text-format.php
@@ -8,83 +8,132 @@
 ?>
 
 
+<h3>Anchor tag</h3>
+
 <p>
 	<a href="#">Test Link Text</a>
 </p>
 
+
+<h3>Abbreviation tag</h3>
 
 <p>
 	<abbr title="National Security Agent">NSA</abbr>
 </p>
 
 
+<h3>Acronym tag</h3>
+
 <p>
 	<acronym title="Persatuan Sepak Bola Bandung">PERSIB</acronym>
 </p>
 
+
+<h3>Address tag</h3>
+
 <p>
-	<address>Jl. Keadilan 3. No. 10 Bandung</address>
+	<address>Jl. Some Street Example, Banundg</address>
 </p>
+
+
+<h3>Bold tag</h3>
 
 <p>
 	<b>The bold text goes here</b>
 </p>
 
+
+
+<h3>Cite tag</h3>
+
 <p>
 	<cite>This is the cited text.</cite>
 </p>
+
+
+<h3>Code tag</h3>
 
 <p>
 	<code>DIV, SPAN, H1</code> <br />
 	<code>div, span, h1, p, b, cite, abbr</code> <br />
 </p>
 
+
+<h3>Delete tag</h3>
+
 <p>
 	<del>This content is deleted</del>
 </p>
+
+
+<h3>Emphasize tag</h3>
 
 <p>
 	<em>signifies emphasized content</em>
 </p>
 
+<h3>Insert tag</h3>
+
 <p>
 	<ins datetime="2013-09-03T00:48:19+00:00">Satuday</ins>
 </p>
+
+<h3>No script tag</h3>
 
 <p>
 	<noscript>provides alternative content for use when scripts aren’t supported or are switched off</noscript>
 </p>
 
+<h3>Pre formatted tag</h3>
+
 <p>
 	<pre>defines preformatted text in which whitespace and line breaks are preserved</pre>
 </p>
+
+
+<h3>Small tag</h3>
 
 <p>
 	<small>sets a reduced font size for the enclosed text</small>
 </p>
 
+<h3>Span tag</h3>
+
 <p>
 	<span>creates a generic inline container</span>
 </p>
+
+
+<h3>Strong tag</h3>
 
 <p>
 	<strong>signifies strongly emphasized content</strong>
 </p>
 
+
+<h3>Subscript tag</h3>
+
 <p>
 	Some texts go here<sub>sub</sub>
 </p>
+
+<h3>Superscript tag</h3>
 
 <p>
 	Some texts go here<sup>sup</sup>
 </p>
 
 
+<h3>Strong tag</h3>
+
 <p><strong>Blockquote</strong></p>
+
+<h3>Quota tag</h3>
 
 <blockquote>
 	The span element is also listed here. Strictly speaking, it’s not a text formatting element, as it doesn’t offer any semantic information on its own. That said, the span can’t be wrapped around a block element, and is primarily used to surround text content for the purposes of styling or scripting.
 </blockquote>
+
 
 <h3 class="subtitle">Single Paragraph Example</h3>
 
