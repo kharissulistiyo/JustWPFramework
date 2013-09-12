334e08a05fa52def172462c66980bbca588e443e
kharissulistiyo <kharisblank@gmail.com>
2013-09-13 03:34:43 +0700

    WP theme files
    Main index template. Still blank! ;D

---
+++ wp-theme/justwpframework/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+
+	<meta charset="UTF-8">
+	<title></title>
+	
+</head>
+<body>
+	
+	
+	<?php wp_head(); ?>
+	
+	
+	
+	
+	<?php wp_footer(); ?>
+	
+</body>
+</html>
