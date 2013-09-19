16e7b982327b40ff7edcb2dd359556333b9c659e
kharissulistiyo <kharisblank@gmail.com>
2013-09-19 19:03:18 +0700

    Wrapper and create header element

--- wp-theme/justwpframework/header.php
+++ wp-theme/justwpframework/header.php
@@ -22,3 +22,38 @@
 <body <?php body_class(); ?>>	
 	
 	
+	<section id="wrapper" class="site">
+	
+	
+		<header id="master-head" class="site-header" role="banner">
+		
+			<!-- Area 1 - This area must be insertable as a framework flexibility design -->
+		
+			<div id="site-identity">
+				<h1 class="site-name"><a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-tagline"><?php bloginfo('description'); ?></h2>
+			</div>
+			
+			<!-- Area 2 - This area must be insertable as a framework flexibility design -->
+			
+			<nav id="main-navigation" class="navigation-menu">
+			
+				<!-- Area 3 - This area must be insertable as a framework flexibility design -->
+			
+				<h3 class="menu-toggler"><?php _e('Main Menu', 'justwpframework'); ?></h3>
+				<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'justwpframework' ); ?>"><?php _e( 'Skip to content', 'justwpframework' ); ?></a>				
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'top-navigation' ) ); ?>
+				
+				<!-- Area 4 - This area must be insertable as a framework flexibility design -->
+				
+			</nav>	
+			
+			<!-- Area 5 - This area must be insertable as a framework flexibility design -->
+			
+			
+			
+			<!-- 
+				It will allow developer to change the main navigation location. Whether before/after site identity
+			-->
+			
+		</header><!-- /#master-head -->
