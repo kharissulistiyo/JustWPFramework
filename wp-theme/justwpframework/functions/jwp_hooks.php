be3d3b1efba228571761e6d878f47b3e26d27974
kharissulistiyo <kharisblank@gmail.com>
2013-09-29 11:58:19 +0700

    Add search form in main menu

--- wp-theme/justwpframework/functions/jwp_hooks.php
+++ wp-theme/justwpframework/functions/jwp_hooks.php
@@ -13,3 +13,10 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 add_action('jwp_header', 'jwp_site_identity', 5 );
 add_action('jwp_header', 'jwp_main_navigation', 10 );
+
+
+/*
+ * Site navigation
+* ============================= */ 
+
+add_action('jwp_after_main_menu', 'jwp_top_search', 1 );
