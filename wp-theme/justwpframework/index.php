ade8b6c723428daabea4ff4431d23c5b9b63f54c
kharissulistiyo <kharisblank@gmail.com>
2013-09-30 04:30:37 +0700

    Create new hooks and define sidebar

--- wp-theme/justwpframework/index.php
+++ wp-theme/justwpframework/index.php
@@ -2,7 +2,7 @@
 
 /*
 * Index file template
-* ===================*/
+* =================== */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
@@ -12,8 +12,26 @@ get_header(); ?>
 
 <div id="content" role="main">
 
+
+	<?php 
+	/* Before post
+	* Hooked: 'jwp_before_post', 'jwp_breadcrumb', 5 => Not implemented yet!
+	* =================== */
+	
+	do_action('jwp_before_post'); ?>
+
+
 	<?php if ( have_posts() ) : ?>
 
+	
+		<?php 
+		/* Before post loop
+		* No hook yet!
+		*/
+		
+		do_action('jwp_before_post_loop'); ?>
+		
+	
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 		
@@ -21,9 +39,29 @@ get_header(); ?>
 			
 		<?php endwhile; ?>
 		
-	<?php endif; ?>	
+		
+		<?php 
+		/* After post loop
+		* Hook 'jwp_after_post_loop', 'jwp_page_navigation', 5
+		*/		
+		
+		do_action('jwp_after_post_loop');?>
+		
+		
+	<?php endif; ?>		
+	
+	
+	<?php 
+	/* After post
+	* No hook yet!
+	* ============ */
+	do_action('jwp_after_post'); ?>
+	
 
 </div><!-- /#content -->
+
+
+<?php get_sidebar(); ?>
 
 
 <?php get_footer(); ?>
