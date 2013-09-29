c6a2cdffab12a45c7dedb771d768fca4e1a492b0
kharissulistiyo <kharisblank@gmail.com>
2013-09-29 13:37:32 +0700

    Add default loop

--- wp-theme/justwpframework/index.php
+++ wp-theme/justwpframework/index.php
@@ -10,11 +10,23 @@ get_header(); ?>
 
 
 
+<div id="content" role="main">
 
+	<?php if ( have_posts() ) : ?>
 
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+		
+			<?php the_title(); ?>
+			<?php the_content(); ?>
+			
+			<hr />
+			
+		<?php endwhile; ?>
+		
+	<?php endif; ?>	
 
-
-
+</div><!-- /#content -->
 
 
 <?php get_footer(); ?>
