c2e7638a6ed41b8f9fa065b61e8a5bd5a801bbea
kharissulistiyo <kharisblank@gmail.com>
2013-09-30 04:27:20 +0700

    Site's footer content

--- wp-theme/justwpframework/footer.php
+++ wp-theme/justwpframework/footer.php
@@ -5,6 +5,22 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 
 
+	
+	<footer id="site-footer">
+	
+	
+		<?php 
+		/* Site footer
+		* Hooked: 'jwp_footer', 'jwp_footer_credit', 30
+		* */
+		
+		do_action('jwp_footer'); ?>
+	
+	
+	</footer><!-- /#site-footer -->
+	
+
+
 	</section><!-- /#wrapper -->
 	
 	<?php wp_footer(); ?>
